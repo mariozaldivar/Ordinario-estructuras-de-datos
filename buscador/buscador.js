3f796548adfc1f49829ec7885d40dc0e9ef22cd6
Rfcc22 <rfconchac@gmail.com>
2026-09-14 20:47:32 -0600

    Anadi el boton de foward y exit en el buscador

--- buscador/buscador.js
+++ buscador/buscador.js
@@ -1,8 +1,9 @@
 class Buscador {
   constructor() {
     this.barra = document.querySelector("#url");
-    this.visitadosElemento = document.querySelector("#visitados");
-    this.retrocedidosElemento = document.querySelector("#retrocedidos");
+    this.visitadosElemento = document.querySelector("#retrocedidos");
+    this.retrocedidosElemento = document.querySelector("#visitados");
+    this.currentPageElememto = document.querySelector("#currentPage");
     this.visitados = [];
     this.retrocedidos = [];
   }
@@ -23,6 +24,13 @@ class Buscador {
       .join("");
   }
 
+  dibujarActual(){
+    const actual = this.visitados[this.visitados.length - 1];
+    this.currentPageElememto.textContent = actual
+      ? `Pagina actual: ${actual}`
+      : "Pagina actual: "
+  }
+
   buscar() {
     if (this.validarURL()) {
       this.visitados.push(this.barra.value);
@@ -30,6 +38,7 @@ class Buscador {
       this.retrocedidos = [];
       this.dibujarVisitados();
       this.dibujarRetrocedidos();
+      this.dibujarActual();
     }
   }
 
@@ -38,16 +47,35 @@ class Buscador {
       this.retrocedidos.push(this.visitados.pop());
       this.dibujarVisitados();
       this.dibujarRetrocedidos();
+      this.dibujarActual();
     }
   }
+
+  avanzar(){
+    if (this.retrocedidos.length !== 0){
+      this.visitados.push(this.retrocedidos.pop());
+      this.dibujarVisitados();
+      this.dibujarRetrocedidos();
+      this.dibujarActual();
+    }
+  }
+
+
+
 }
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const buscador = new Buscador();
 
   const botonBuscar = document.querySelector("#buscar");
   const botonRetroceder = document.querySelector("#retroceder");
+  const botonExit = document.querySelector("#exit");
+  const botonAvanzar = document.querySelector("#avanzar");
 
   botonBuscar.addEventListener("click", () => buscador.buscar());
   botonRetroceder.addEventListener("click", () => buscador.retroceder());
+  botonAvanzar.addEventListener("click",() => buscador.avanzar());
+  botonExit.addEventListener("click", () => window.close());
 });
